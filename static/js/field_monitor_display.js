8065ac59cf702a440f4b70bf9af2bbeb9836cea3
Corey Applegate <cpapplefamily@gmail.com>
2024-07-19 10:47:54 -0500

    Added Game Sounds to Driver Station Status display

--- static/js/field_monitor_display.js
+++ static/js/field_monitor_display.js
@@ -161,6 +161,16 @@ var handleMatchTime = function(data) {
   });
 };
 
+// Handles a websocket message to play a sound to signal match start/stop/etc.
+const handlePlaySound = function(sound) {
+  $("audio").each(function(k, v) {
+    // Stop and reset any sounds that are still playing.
+    v.pause();
+    v.currentTime = 0;
+  });
+  $("#sound-" + sound)[0].play();
+};
+
 // Handles a websocket message to update the match score.
 var handleRealtimeScore = function(data,reversed) {
 
@@ -251,5 +261,6 @@ $(function() {
     matchTiming: function(event) { handleMatchTiming(event.data); },
     matchTime: function(event) { handleMatchTime(event.data); },
     realtimeScore: function(event) { handleRealtimeScore(event.data,reversed); },
+    playSound: function(event) { handlePlaySound(event.data); },
   });
 });
