2c99b26264701a5040e744150aba430c601721ab
justfishin1999 <107942195+justfishin1999@users.noreply.github.com>
2025-05-28 11:57:09 -0500

    add help page for driver station displays, modify field monitor help page to include nav bar and cleanup css

--- web/web.go
+++ web/web.go
@@ -235,9 +235,10 @@ func (web *Web) newHandler() http.Handler {
 	mux.HandleFunc("GET /setup/teams/generate_wpa_keys", web.teamsGenerateWpaKeysHandler)
 	mux.HandleFunc("GET /setup/teams/progress", web.teamsUpdateProgressBarHandler)
 	mux.HandleFunc("GET /setup/teams/refresh", web.teamsRefreshHandler)
-	
+
 	// Freezy Arena
 	mux.HandleFunc("GET /help/freezy/field_monitor_help", web.fieldMonitorDisplayHelpHandler)
+	mux.HandleFunc("GET /help/freezy/alliance_station_display_help", web.allianceStationDisplayHelpHandler)
 	mux.HandleFunc("GET /api/freezy/allianceStatus", web.allianceStatusApiHandler)
 	mux.HandleFunc("POST /api/freezy/eStopState", web.eStopStatePostHandler)
 	mux.HandleFunc("GET /panel/freezy/eStopControl/{alliance}", web.estopContolDisplayHandler)
